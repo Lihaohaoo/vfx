08d464003b33d676503ea519c54adbfb482963fd
Lihaohaoo <122856244+Lihaohaoo@users.noreply.github.com>
2024-11-03 23:31:33 +0800

    1
    1

--- Assets/FX_assest/15/AFX_SkinRandomLight.cs
+++ Assets/FX_assest/15/AFX_SkinRandomLight.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-//[ExecuteAlways]
+[ExecuteAlways]
 public class AFX_SkinRandomLight : MonoBehaviour
 {
     [Header("······全身电弧功能········")]
