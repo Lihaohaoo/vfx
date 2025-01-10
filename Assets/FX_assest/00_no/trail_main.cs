0eaa548a4fc494333d9f593fa0845c24db6755e6
Lihaohaoo <1763787649@qq.com>
2025-01-10 19:11:56 +0800

    1
    1

--- Assets/FX_assest/00_no/trail_main.cs
+++ Assets/FX_assest/00_no/trail_main.cs
@@ -3,78 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteAlways]
-public class trail_main : MonoBehaviour
+public class trail_manin : MonoBehaviour
 {
-    private Material mat;
-    private Material mat_m;
+    Vector3 poslow;
+    Vector3 posnew;
+    Material mat;
+    float dis;
+    public float offset;
+    public Vector3 lowpos;
+    public float offsetSize;
     private Vector4 Main_t;
-    private float zValue;
-    private float offset;
-    private float offsetSize;
-    private Vector3 poslow;
-    private Vector3 posnew;
-    private Vector3 lowpos;
-    private bool isemiiting;
-    public float dissolve_start = 0;
-    public float dissolve_end = 1;
-    public float dissolve_time ;
-    float tim;
-    float Diss;
-    
-    private  new TrailRenderer renderer;
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        renderer = GetComponent<TrailRenderer>();
-        dissolve_time = 1 /  renderer.time;
-        mat_m = GetComponent<TrailRenderer>().material;
-        isemiiting = renderer.emitting;
-        mat = mat_m;
         lowpos = transform.position;
-        Main_t = mat.GetVector("_Main_Tex");
-        
+        mat = gameObject.GetComponent<Renderer>().material;
+        //Main_t = mat.GetVector("_Main_Tex");
     }
 
-    private void LateUpdate()
-    {
-        isemiiting = renderer.emitting;
-        Mat_offset();
-        
-    }
-
-    void Mat_offset()
+    // Update is called once per frame
+    void Update()
     {
         poslow = lowpos;
         OnLowPosSave();
-        offset = 0 - Main_t.x;
+        //offset = 0 - Main_t.x;
         posnew = transform.position;
-        float distance = Vector3.Distance(poslow, posnew);
-        offsetSize += distance;
-        zValue = offsetSize * offset;
-        Main_t.z = zValue; // 修改 z 分量
-        if(isemiiting == true)
-        {
-            mat.SetVector("_Main_Tex", Main_t); // 更新材质属性
+        dis = Vector3.Distance(poslow, posnew);
+        offsetSize += dis;
+        mat.SetFloat("_offset_U", offsetSize * offset);
 
-            if (offsetSize >= 1 / Main_t.x * 2)
-            {
-                offsetSize = offsetSize - 1 / Main_t.x * 2;
-            }
-        }
-
-        if (isemiiting == false)
-        {
-            if (dissolve_time > 0f)
-            {
-                tim += Time.deltaTime * dissolve_time;
-                Diss = Mathf.Lerp(dissolve_start, dissolve_end, tim);
-                mat.SetFloat("_Dissolve", Diss); // 更新材质属性
-                                                 //Debug.Log("disssss" + tim);
-                if (tim >= 1)
-                {
-                    dissolve_time = 0f; // 重置 dissolve_time 值
-                }
-            }
-        }
 
     }
     void OnLowPosSave()
